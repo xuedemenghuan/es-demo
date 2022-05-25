91080389dda186d899d75943e0f90fbff5efe120
zhushuainan <943799879@qq.com>
2022-05-26 01:28:30 +0800

    Initial commit

---
+++ src/test/java/cn/itcast/hotel/HotelDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.itcast.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
---
+++ src/test/java/cn/itcast/hotel/HotelDocumentTest.java
@@ -0,0 +1,212 @@
+package cn.itcast.hotel;
+
+import cn.itcast.hotel.pojo.Hotel;
+import cn.itcast.hotel.pojo.HotelDoc;
+import cn.itcast.hotel.service.IHotelService;
+import com.alibaba.fastjson.JSON;
+import org.apache.http.HttpHost;
+import org.apache.lucene.search.TotalHits;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author zsn
+ * @CreateTime: 2022/5/11 22:25
+ * @Description:
+ */
+@SpringBootTest
+public class HotelDocumentTest {
+    @Autowired
+    private IHotelService hotelService;
+
+    private RestHighLevelClient client;
+
+    @BeforeEach
+    public void setUp() {
+        this.client = new RestHighLevelClient
+                (RestClient.builder(HttpHost.create("http://192.168.188.129:9200")));
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException {
+        this.client.close();
+    }
+
+    @Test
+    void testAddDocument() throws IOException {
+        // 1.根据id查询酒店数据
+        Hotel hotel = hotelService.getById(61083L);
+        // 2.转为文档类型
+        HotelDoc hotelDoc = new HotelDoc(hotel);
+        // 3.将HotelDoc转json
+        String json = JSON.toJSONString(hotelDoc);
+
+        // 1.准备Request对象
+        IndexRequest request = new IndexRequest("hotel").id(hotelDoc.getId().toString());
+        //2.准备Json文档
+        request.source(json, XContentType.JSON);
+        //3.发送请求
+        client.index(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    public void testGetDocumentById() throws IOException {
+        // 1.准备Request
+        GetRequest request = new GetRequest("hotel", "61083");
+        // 2.发送请求，得到响应
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        // 3.解析响应结果
+        String json = response.getSourceAsString();
+
+        HotelDoc hotelDoc = JSON.parseObject(json, HotelDoc.class);
+        System.out.println(hotelDoc);
+    }
+
+    @Test
+    void testDeleteDocument() throws IOException {
+        // 1.准备Request
+        DeleteRequest request = new DeleteRequest("hotel", "61083");
+        // 2.发送请求
+        client.delete(request,RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testUpdateDocument() throws IOException {
+        // 1.准备Request
+        UpdateRequest request = new UpdateRequest("hotel", "61083");
+        // 2.准备请求参数
+        request.doc(
+                "price","952",
+                "startName","四钻"
+        );
+        // 3.发送请求
+        client.update(request,RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testBulkRequest() throws IOException {
+        // 批量查询酒店
+        List<Hotel> hotels = hotelService.list();
+        // 1.创建Request
+        BulkRequest request = new BulkRequest();
+        // 2.准备参数，添加多个新增的Request
+        for (Hotel hotel : hotels) {
+            // 2.1.转换为文档类型HotelDoc
+            HotelDoc hotelDoc = new HotelDoc(hotel);
+            // 2.2.创建新增文档的Request对象
+            request.add(new IndexRequest("hotel")
+                    .id(hotelDoc.getId().toString())
+                    .source(JSON.toJSONString(hotelDoc), XContentType.JSON));
+            // 3.发送请求
+            client.bulk(request,RequestOptions.DEFAULT);
+        }
+
+    }
+
+    @Test
+    public void testMatchAll() throws IOException{
+        // 1.准备Request
+        SearchRequest request = new SearchRequest("hotel");
+        // 2.组织DSL参数
+        request.source().query(QueryBuilders.matchAllQuery());
+        // 3.发送请求，得到响应结果
+        SearchResponse search = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(search);
+
+    }
+
+    private void handleResponse(SearchResponse response){
+        // 4.解析响应
+        SearchHits searchHits = response.getHits();
+        // 4.1获取总条数
+        TotalHits totalHits = searchHits.getTotalHits();
+        System.out.println("共搜索到" + totalHits + "条数据");
+        // 4.2文档数组
+        SearchHit[] hits = searchHits.getHits();
+        for (SearchHit hit : hits) {
+            // 获取文档source
+            String json = hit.getSourceAsString();
+            // 反序列化
+            HotelDoc hotelDoc = JSON.parseObject(json, HotelDoc.class);
+            System.out.println("hotelDoc = "+hotelDoc);
+        }
+    }
+
+    @Test
+    void testMatch() throws IOException {
+        // 1.准备Request
+        SearchRequest searchRequest = new SearchRequest("hotel");
+        // 2.准备DSL
+        searchRequest.source()
+                .query(QueryBuilders.matchQuery("all","如家"));
+        // 3.发送请求
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    @Test
+    void testBool() throws IOException {
+        // 1.准备Request
+        SearchRequest request = new SearchRequest("hotel");
+        // 2.准备DSL
+        // 2.1.准备BooleanQuery
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+        // 2.2添加term
+        boolQuery.must(QueryBuilders.termQuery("city","杭州"));
+        // 2.3添加range
+        boolQuery.filter(QueryBuilders.rangeQuery("price").lte(250));
+
+        request.source().query(boolQuery);
+        // 3.发送请求
+        SearchResponse search = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(search);
+    }
+
+    @Test
+    void testPageAndSort() throws IOException {
+        // 页码，每页大小
+        int page = 1, size = 5;
+
+        // 1.准备Request
+        SearchRequest request = new SearchRequest("hotel");
+        // 2.准备DSL
+        // 2.1.query
+        request.source().query(QueryBuilders.matchAllQuery());
+        // 2.2.排序 sort
+        request.source().sort("price", SortOrder.ASC);
+        // 2.3.分页 from、size
+        request.source().from((page -1) * size).size(5);
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+}
+
